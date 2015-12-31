cb44283e5394fb5ec4e81b9ccb5dfb5edb5c4f22
cinjon <cinjon.resnick@gmail.com>
2015-12-31 17:46:46 +0000

    config: use the shuffled datasets ... and fix the dicts_fr_tok to point at the french dict, not the english one

--- session2/config.py
+++ session2/config.py
@@ -2,11 +2,11 @@ import os
 
 base_dir = '/home/ubuntu/research/dl4mt-material/'
 
-train_en_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.en.tok')
-train_fr_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.fr.tok')
+train_en_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.en.tok.shuf')
+train_fr_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.fr.tok.shuf')
 
-valid_en_tok = os.path.join(base_dir, 'files', 'newstest2013.en.tok')
-valid_fr_tok = os.path.join(base_dir, 'files', 'newstest2013.fr.tok')
+valid_en_tok = os.path.join(base_dir, 'files', 'newstest2013.en.tok.shuf')
+valid_fr_tok = os.path.join(base_dir, 'files', 'newstest2013.fr.tok.shuf')
 
-dicts_en_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.en.tok.pkl')
-dicts_fr_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.en.tok.pkl')
+dicts_en_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.en.tok.shuf.pkl')
+dicts_fr_tok = os.path.join(base_dir, 'files', 'europarl-v7.fr-en.fr.tok.shuf.pkl')
